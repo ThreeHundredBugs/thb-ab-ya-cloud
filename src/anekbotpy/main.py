1845fc5ec29d1e9800a35ef6147ec3f62bd34424
Nikolay Z <zver20nik00@gmail.com>
2024-09-23 21:50:37 +0700

    Change swear handler

--- src/anekbotpy/main.py
+++ src/anekbotpy/main.py
@@ -6,12 +6,11 @@ from telegram import Bot
 from anekbotpy import handle_event
 
 _BOT_TOKEN = os.environ['BOT_TOKEN']
-_BOT = None
 
 
 async def main(event: dict, context):
     tg_event = json.loads(event['body'])
-    print(tg_event)
+    print(json.dumps(tg_event))
 
     bot = Bot(token=_BOT_TOKEN)
     await handle_event(bot, tg_event)
